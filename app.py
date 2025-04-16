258ed28d2b4b03c2274a51fe070da27c3a094340
Sasmue94 <sascha@mueller1000.de>
2025-04-16 16:13:46 +0200

    created ipv4 class and finished frontend

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
+from ipv4 import IPv4
 import re
 
 # globals
-pattern = re.compile(r'^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$')
+__pattern = re.compile(r'^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$')
 
-def isValidID(id: str, pattern: re.Pattern) -> bool:
+def is_valid_id(id: str, pattern: re.Pattern) -> bool:
     return bool(pattern.match(id))
 
 def control(condition: bool, onError, msg: str) -> None:
@@ -12,56 +13,32 @@ def control(condition: bool, onError, msg: str) -> None:
         st.error(msg)
         onError()
 
-def convertNetmask(netmask: int) -> list[str]:
-    zeros = 32 - netmask
-    convertedMask = []
-    octet = ""
-    while netmask > 0 or zeros > 0:
-        if netmask > 0:
-            octet = octet + "1"
-            netmask -= 1
-        elif zeros > 0:
-            octet = octet + "0"
-            zeros -= 1
-        if len(octet) == 8:
-            convertedMask.append(octet)
-            octet = ""
-    return convertedMask
-
-def convertToBits(num: int) -> str:
-    return "{0:08b}".format(int(num, base=10))
-
-def convertIP(addr: str) -> list[str]:
-    parts = addr.split(".")
-    for idx, e in enumerate(parts):
-        parts[idx] = "{0:08b}".format(int(e, base=10))
-    return parts
-
-def getNetID(addr: list[str], mask: list[str]) -> str:
-    idOctets = []
-    for idx, e in enumerate(addr):
-        idOctets.append(int(addr[idx], base=2) & int(mask[idx], base=2))
-    return idOctets
-    
-
-
 # header area
 st.header("IPv4 Subnet Calculator")
 
 # input form
 with st.form(key="calculator_form", enter_to_submit=False):
-    st.subheader("Please insert your IPv4 NetID and Netmask")
+    st.subheader("Please insert your IPv4 Address of Net-Id and Prefix")
     address = st.text_input("IPv4 Address or NetID", placeholder="192.168.0.0", key="ID")
-    curMask = st.number_input("current netmask", min_value=0, max_value=32, value=24, step=1, key="current_mask")
-    tarMask = st.number_input("target netmask", min_value=0, max_value=32, value=25, step=1, key="target_mask")
+    current_prefix = st.number_input("current Prefix", min_value=0, max_value=32, value=24, step=1, key="current_prefix")
+    target_prefix = st.number_input("target Prefix", min_value=0, max_value=32, value=25, step=1, key="target_prefix")
 
 
     submit = st.form_submit_button("Calculate")
     if submit:
-        control(condition=not isValidID(id=address, pattern=pattern), onError=st.stop, msg="Not a valid NetID")
-        control(condition= tarMask <= curMask, onError=st.stop, msg="target Netmask has to be bigger than current Netmask")
-        convertedIP = convertIP(addr=address)
-        convertedMask = convertNetmask(netmask=curMask)
-        st.write(convertedIP)
-        netID = getNetID(addr=convertedIP, mask=convertedMask)
-        st.write(netID)
+        control(condition=not is_valid_id(id=address, pattern=__pattern), onError=st.stop, msg="Not a valid NetID")
+        control(condition= target_prefix <= current_prefix, onError=st.stop, msg="target Netmask has to be bigger than current Netmask")
+        ip = IPv4(ip=address, prefix=current_prefix)
+        st.divider()
+        st.write(f"current IPv4-Network: {ip.get_net_id()}/{ip.get_prefix()}")
+        st.write(f"current Prefix evaluates to the following Subnetmask: {ip.get_dec_mask()}")
+        st.write(f"current Number of hosts: {ip.get_current_net_size() - 2}")
+        st.divider()
+        st.subheader("After subnetting the following applies:")
+        st.write(f"Number of hosts per network: {ip.get_target_net_size(target_prefix=target_prefix) - 2}")
+        st.write(f"Number of networks: {ip.get_no_of_subnets(target_prefix=target_prefix)}")
+        st.divider()
+        st.subheader("List of Subnets:")
+        subnets = ip.subnet(target_prefix=target_prefix)
+        for subnet in subnets:
+            st.write(subnet)
---
+++ ipv4.py
@@ -0,0 +1,122 @@
+import re
+class IPv4:
+    def __init__(self, ip: str, prefix: int):
+        self.ip = ip
+        self.octet_list = ip.split(".")
+        self.prefix = prefix
+        self.__bitlength = 32
+        self.__pattern = re.compile(r'^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$')
+    
+    def set_ip(self, ip: str):
+        if bool(self.__pattern.match(ip)):
+            self.ip = ip
+            self.octet_list = ip.split(".")
+        else:
+            raise ValueError(f"{ip} is not a valid ip in range of 0.0.0.0 - 255.255.255.255")
+    
+    def get_ip(self) -> str:
+        return self.ip
+    
+    def get_prefix(self) -> int:
+        return self.prefix
+
+    def get_bin_octet(self, idx: int) -> str:
+        if idx >= 0 and idx < 4:
+            return "{0:08b}".format(int(self.octet_list[idx], base=10))
+        else:
+            raise IndexError("Index out of range of ipv4: 0 - 3")
+    
+    def get_bin_ip(self):
+        parts = []
+        for e in self.octet_list:
+            parts.append("{0:08b}".format(int(e)))
+        return parts
+    
+    def get_net_id(self) -> str:
+        mask = self.get_bin_mask()
+        idOctets = ""
+        for idx, e in enumerate(self.octet_list):
+            idOctets += str(int(self.octet_list[idx]) & int(mask[idx], base=2))
+            if idx < 3:
+                idOctets += "."
+        return idOctets
+    
+    def get_net_id_bin(self) -> list[str]:
+        net_id = self.get_net_id().split(".")
+        for idx, oct in enumerate(net_id):
+            net_id[idx] = "{0:08b}".format(int(oct))
+        return net_id
+    
+    def get_bin_mask(self) -> list[str]:
+        zeros = self.__bitlength - self.prefix
+        netmask = self.prefix
+        convertedMask = []
+        octet = ""
+        while netmask > 0 or zeros > 0:
+            if netmask > 0:
+                octet = octet + "1"
+                netmask -= 1
+            elif zeros > 0:
+                octet = octet + "0"
+                zeros -= 1
+            if len(octet) == 8:
+                convertedMask.append(octet)
+                octet = ""
+        return convertedMask
+    
+    def get_dec_mask(self) -> str:
+        mask = self.get_bin_mask()
+        mask_octets = ""
+        for idx, e in enumerate(mask):
+            mask_octets += str(int(e, base=2))
+            if idx < 3:
+                mask_octets += "."
+        return mask_octets
+    
+    def get_current_net_size(self) -> int:
+        return 2 ** (self.__bitlength - self.get_prefix())
+    
+    def get_target_net_size(self, target_prefix: int) -> int:
+        return 2 ** (self.__bitlength - target_prefix)
+    
+    def get_no_of_subnets(self, target_prefix: int) -> int:
+        return int(self.get_current_net_size() / self.get_target_net_size(target_prefix=target_prefix))
+    
+    def convert_prefix_to_bin(self, mask: int) -> list[str]:
+        zeros = self.__bitlength - mask
+        convertedMask = []
+        octet = ""
+        while mask > 0 or zeros > 0:
+            if mask > 0:
+                octet = octet + "1"
+                mask -= 1
+            elif zeros > 0:
+                octet = octet + "0"
+                zeros -= 1
+            if len(octet) == 8:
+                convertedMask.append(octet)
+                octet = ""
+        return convertedMask
+
+    def subnet(self, target_prefix: int) -> list[int]:
+        subnets = []
+        no_of_nets = self.get_no_of_subnets(target_prefix=target_prefix)
+        net_size = self.get_target_net_size(target_prefix=target_prefix)
+        net_id_list = self.get_net_id_bin()
+        net_id_bin = ""
+        for oct in net_id_list:
+            net_id_bin += oct
+        i = 0
+        net_id_bin = int(net_id_bin, 2)
+        while i < no_of_nets:
+            suffix = i * net_size
+            subnet = "{0:032b}".format(int(net_id_bin | suffix))
+            subnet = f"{int(subnet[0:8], 2)}.{int(subnet[8:16], 2)}.{int(subnet[16:24], 2)}.{int(subnet[24:32], 2)} /{target_prefix}"
+            subnets.append(subnet)
+            i += 1   
+
+        return subnets 
+        
+        
+
+
